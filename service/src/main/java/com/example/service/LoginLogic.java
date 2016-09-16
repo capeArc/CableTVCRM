f405655cac892bb9f24bea8184d776f8ee310032
Pavan <pavan.jadhao@capearc.com>
2016-09-16 13:49:08 +0530

    Anim Folder -->left & right file added Update LoginViewModel file with animation

---
+++ service/src/main/java/com/example/service/LoginLogic.java
@@ -0,0 +1,26 @@
+package com.example.service;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Pavan on 9/16/2016.
+ */
+public class LoginLogic extends AsyncTask<String,Void,Void > {
+
+
+    static   String result;
+
+    @Override
+    protected Void doInBackground(String... params) {
+
+        //code for  Web Api
+
+        result="ok";
+
+        return null;
+    }
+
+
+
+
+}
